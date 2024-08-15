08ef14dc28a3974220b3ece395252bced9edba46
Ben <ben.d.godfrey2002@gmail.com>
2024-08-15 10:19:59 +0100

    Added point button

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 function operation(operationList){
     switch(operationList[1]){
-        case "add":
+        case "plus":
             return operationList[0] + operationList[2];
             break;
-        case "subtract":
+        case "minus":
             return operationList[0] - operationList[2];
             break;
-        case "multiply":
+        case "times":
             return operationList[0] * operationList[2];
             break;
         case "divide":
@@ -16,26 +16,48 @@ function operation(operationList){
 }
 
 let operationList = [];
+const displayNumber = document.querySelector("#displayNumber")
 
 //currentNum holds the sequence of numbers pressed since last operation
 //when an operation is selected, parse to int and add to operationList
-let currentNum = "";
+let currentNum = "0";
 
 let numbers = document.getElementsByClassName("number");
 Array.from(numbers).forEach((numButton) => {
-    numButton.addEventListener("click", (numButton) => {
-        console.log(numButton.target.id);
-        currentNum += numButton.target.id;
+    numButton.addEventListener("click", () => {
+        console.log(numButton.id);
+        if (currentNum == "0"){
+            currentNum = numButton.id;
+        }
+        else{
+            currentNum += numButton.id;
+        }
+        
         
         //display currentNum
-        let displayNumber = document.querySelector("#displayNumber");
         displayNumber.textContent = currentNum;
-    })
-})
+    });
+});
 
 const ac = document.querySelector("#AC");
 ac.addEventListener("click", () => {
     currentNum = "";
     operationList = [];
     displayNumber.textContent = currentNum;
-})
+});
+
+
+const point = document.querySelector("#point");
+point.addEventListener("click", () => {
+    if (currentNum.slice(-1) == "."){
+        currentNum = currentNum.slice(0, -1);
+        displayNumber.textContent = currentNum;
+    }
+    else if (currentNum.includes(".")){
+        displayNumber.textContent = currentNum;
+    }
+    else{
+        currentNum += "."
+        displayNumber.textContent = currentNum;
+    }
+});
